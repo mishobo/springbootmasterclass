4b1a3478cfd1a0506f1f4b2e79bfea0c1e37ff63
mishobo <husseinabdalla287@gmail.com>
2024-08-06 07:25:17 +0300

    first commit

---
+++ src/main/java/com/husseinabdallah/SpringBootMasterClass/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.husseinabdallah.SpringBootMasterClass.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class Controller {
+
+    @GetMapping(value = "hello")
+    public String hello(){
+        return "hello spring boot";
+    }
+}
---
+++ src/test/java/com/husseinabdallah/SpringBootMasterClass/SpringBootMasterClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.husseinabdallah.SpringBootMasterClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMasterClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
